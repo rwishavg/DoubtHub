8db8276740b541f28d6c4bccb8087a36d3d0354b
rwishavg <grwishav34@gmail.com>
2022-02-02 22:29:15 +0530

    logout ui

--- client/src/Components/DashboardComponents/Sidebar.jsx
+++ client/src/Components/DashboardComponents/Sidebar.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Logo from "../../Assets/DoubtHub Logo.png";
 import { Link } from "react-router-dom";
+
+const api_endpoint = process.env.REACT_APP_API_ENDPOINT;
+
 const Sidebar = (props) => {
 	return (
 		<div className="sidebarContainer">
@@ -8,13 +11,15 @@ const Sidebar = (props) => {
 				<img src={Logo} alt="" className="logo" />
 				<Link to="./test">test</Link>
 				<Link to="/dashboard">dashboard</Link>
+				<a style={{position:"absolute", bottom:"120px"}}className="button" href={api_endpoint + "/user/logout"}>
+					Logout
+				</a>
 				<div className="cardComponent userCard">
 					<img
 						src={props.user.picture}
 						alt=""
 						className="userPhoto"
 					/>
-
 					<div className="name">
 						{props.user.given_name} {props.user.family_name}
 					</div>
