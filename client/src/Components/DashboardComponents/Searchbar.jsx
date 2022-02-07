5e0b0b45d74704b5241fb52ae1daf2521253f3fb
rwishavg <grwishav34@gmail.com>
2022-02-07 21:47:08 +0530

    states for profile tag filter

--- client/src/Components/DashboardComponents/Searchbar.jsx
+++ client/src/Components/DashboardComponents/Searchbar.jsx
@@ -1,16 +1,30 @@
-import React from "react";
+import React,{ useState, useEffect } from "react";
 import classes from "../../Styles/component-styles/searchbar.module.css";
 import search from "../../Assets/Icons/search.svg";
 import filter from "../../Assets/Icons/filter.svg";
+
+const tagNames = ["TAG", "USERNAME", "FULL NAME", "QUESTION"];
+
 const Searchbar = () => {
+	const [counter, setCounter] = useState(0);
+	const [filterState, setFilterState] = useState(["TAG"]);
+	
+	const changeTag = () => {
+		setCounter((counter + 1)%tagNames.length);
+	}
+
+	useEffect(() => {
+		setFilterState (tagNames[counter]);
+	}, [counter]);
+	
 	return (
 		<div className={`cardComponent ${classes.searchComponent}`}>
 			<div className={`${classes.background}`}>
 				<img src={search} alt="" className={classes["searchIcon"]} />
-				{/* Name: */}
+				{/* Name: */}<div className={classes["searchTag"]}>{filterState}</div>
 				<input type="text" className={classes["searchInput"]} />
 			</div>
-			<div className={`${classes.filterBg}`}>
+			<div className={`${classes.filterBg}`} onClick={changeTag}>
 				<img src={filter} alt="" className={classes["filterIcon"]} />
 			</div>
 		</div>
