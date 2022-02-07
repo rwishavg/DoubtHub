52ee5ee73450f891092cc1c80a0e8107f7f9eb17
Raghav Mathur <raghavmathur@Raghavs-MBP.bbrouter>
2022-02-07 22:14:15 +0530

    Search UI Complete

--- client/src/Components/DashboardComponents/Searchbar.jsx
+++ client/src/Components/DashboardComponents/Searchbar.jsx
@@ -1,28 +1,40 @@
-import React,{ useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import classes from "../../Styles/component-styles/searchbar.module.css";
 import search from "../../Assets/Icons/search.svg";
 import filter from "../../Assets/Icons/filter.svg";
 
-const tagNames = ["TAG", "USERNAME", "FULL NAME", "QUESTION"];
+const tagNames = ["ALL", "TAG", "USERNAME", "NAME", "QUESTION"];
 
 const Searchbar = () => {
 	const [counter, setCounter] = useState(0);
 	const [filterState, setFilterState] = useState(["TAG"]);
-	
+	const [searchText, setSearchText] = useState("");
 	const changeTag = () => {
-		setCounter((counter + 1)%tagNames.length);
-	}
+		setCounter((counter + 1) % tagNames.length);
+	};
 
 	useEffect(() => {
-		setFilterState (tagNames[counter]);
+		setFilterState(tagNames[counter]);
 	}, [counter]);
-	
+
 	return (
 		<div className={`cardComponent ${classes.searchComponent}`}>
 			<div className={`${classes.background}`}>
-				<img src={search} alt="" className={classes["searchIcon"]} />
-				{/* Name: */}<div className={classes["searchTag"]}>{filterState}</div>
-				<input type="text" className={classes["searchInput"]} />
+				{/* Name: */}
+				<div className={classes["searchTag"]}>
+					<img
+						src={search}
+						alt=""
+						className={classes["searchIcon"]}
+					/>
+					{filterState}
+				</div>
+
+				<input
+					type="text"
+					className={classes["searchInput"]}
+					onChange={(e) => setSearchText(e.target.value)}
+				/>
 			</div>
 			<div className={`${classes.filterBg}`} onClick={changeTag}>
 				<img src={filter} alt="" className={classes["filterIcon"]} />
