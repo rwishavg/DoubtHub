d992e22299d0a220df0362449e038797bcd2bf63
Raghav Mathur <raghavmathur@Raghavs-MacBook-Pro.local>
2022-02-12 20:41:14 +0530

    Landing Page added

--- client/src/Pages/Landing.jsx
+++ client/src/Pages/Landing.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import Logo from "../Assets/DoubtHub Logo.png";
+import "../Styles/page-styles/landing.css";
 const Landing = () => {
-  return (
-    <div style={{ marginTop:"40vh", marginLeft:"45vw", width:"200px"}}>
-      <Link to="/login" className="button">
+	return (
+		<div className="landingContainer">
+			<div className="logoLanding">
+				<img src={Logo} alt="" />
+			</div>
+			<div className="loginSign">
+				<Link to="/login" className="button">
 					Login
-      </Link>
-      <Link to="/register" className="button">
+				</Link>
+				<Link to="/register" className="button">
 					Sign Up
-			</Link>
-    </div>
-  );
+				</Link>
+			</div>
+		</div>
+	);
 };
 
 export default Landing;
