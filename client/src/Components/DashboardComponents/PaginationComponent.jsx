432023393999053453cf0ba4ddc545774eda1df3
Raghav Mathur <raghav3501@gmail.com>
2022-03-07 23:04:44 +0530

    Pagination Bug Remove

--- client/src/Components/DashboardComponents/PaginationComponent.jsx
+++ client/src/Components/DashboardComponents/PaginationComponent.jsx
@@ -6,6 +6,7 @@ import { UilAngleRight } from "@iconscout/react-unicons";
 import { UilAngleLeft } from "@iconscout/react-unicons";
 import classes from "../../Styles/component-styles/pagination.module.css";
 import { motion } from "framer-motion";
+import { number } from "prop-types";
 
 const variants = {
 	open: {
@@ -17,39 +18,42 @@ const variants = {
 };
 const PaginationComponent = (props) => {
 	const [isOpen, setIsOpen] = useState(false);
-	return (
-		<div className={classes["container"]}>
-			<motion.div
-				animate={isOpen ? "open" : "closed"}
-				variants={variants}
-				transition={{
-					duration: 0.4,
-				}}
-			>
-				<div className={classes["number"]}>{props.number}</div>
-			</motion.div>
-			<Link
-				to={`/${props.page}/${props.number - 1}`}
-				className={classes["links"]}
-			>
-				<UilAngleLeft
-					color="black"
-					onClick={() => setIsOpen((isOpen) => !isOpen)}
-				/>
-			</Link>
-			{props.size === 10 && (
+	if (props.number == 1 && props.size < 10) {
+		return <div></div>;
+	} else
+		return (
+			<div className={classes["container"]}>
+				<motion.div
+					animate={isOpen ? "open" : "closed"}
+					variants={variants}
+					transition={{
+						duration: 0.4,
+					}}
+				>
+					<div className={classes["number"]}>{props.number}</div>
+				</motion.div>
 				<Link
-					to={`/${props.page}/${props.number + 1}`}
+					to={`/${props.page}/${props.number - 1}`}
 					className={classes["links"]}
 				>
-					<UilAngleRight
+					<UilAngleLeft
 						color="black"
 						onClick={() => setIsOpen((isOpen) => !isOpen)}
 					/>
 				</Link>
-			)}
-		</div>
-	);
+				{props.size === 10 && (
+					<Link
+						to={`/${props.page}/${props.number + 1}`}
+						className={classes["links"]}
+					>
+						<UilAngleRight
+							color="black"
+							onClick={() => setIsOpen((isOpen) => !isOpen)}
+						/>
+					</Link>
+				)}
+			</div>
+		);
 };
 
 export default PaginationComponent;
