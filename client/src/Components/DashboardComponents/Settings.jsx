deddadab336c4e03793fa1207fcfdbb5995fe13f
rwishavg <grwishav34@gmail.com>
2022-03-18 12:26:55 +0530

    Password Change backend + DarkMode/LightMode switch completed

--- client/src/Components/DashboardComponents/Settings.jsx
+++ client/src/Components/DashboardComponents/Settings.jsx
@@ -1,12 +1,11 @@
-import React, { useContext, useState} from "react";
-import "../../Styles/component-styles/profile.css";
+import React, { useContext, useState } from "react";
+import "../../Styles/component-styles/settings.css";
 import axios from "axios";
 import { userObjectContext } from "../../Context";
 import { toast } from "wc-toast";
 import Input from "./Input";
 import { UilEye } from "@iconscout/react-unicons";
 import DarkMode from "../DashboardComponents/DarkMode";
-
 
 const api_endpoint = process.env.REACT_APP_API_ENDPOINT;
 
@@ -17,70 +16,101 @@ const Settings = () => {
 	const [reNewPassword, setReNewPassword] = useState("");
 
 	const changePass = () => {
+		if (newPassword === reNewPassword) {
+			axios({
+				method: "PUT",
+				data: {
+					oldPassword: password,
+					newPassword: newPassword,
+					emailID: user.emailID,
+				},
+				withCredentials: true,
+				url: api_endpoint + "/user/profile/changePassword",
+			}).then((response) => {
+				toast.success("Password Changed", response);
+			});
+		} else {
+			toast.error("Passwords do not match");
+		}
+	};
+
+	console.log(user);
+
+	const deleteAccount = () => {
 		axios({
-			method: "PUT",
+			method: "DELETE",
 			data: {
-				oldPassword: password,
-				newPassword: newPassword,
-				reNewPassword: reNewPassword,
+				password: password,
 				email: user.emailID,
 			},
 			withCredentials: true,
-			url: api_endpoint + "/user/profile/password",
+			url: api_endpoint + "/user/profile/deleteAccount",
 		}).then((response) => {
-			toast.success("Password Changed");
+			toast.success("Account Deleted");
 		});
 	};
 
 	return (
-		<div className="cardComponent profileCard">
-			<form className="profileForm">
+		<div className="cardComponent settingsCard">
+			<div className="settingsForm">
 				<div className="heading">Settings</div>
+				<div className="heading2">Toggle LightMode/ DarkMode :</div>
 				<DarkMode />
-				<div className="profileName">
-					<div className="nameSize">
-						<div className="heading2">Password Change :</div>
-						<Input
-							heading="Current Password"
-							placeholder="Password"
-							update={setPassword}
-							type="password"
-						>
-							<UilEye color="#A0A3BD" />
-						</Input>
-						<Input
-							heading="New Password"
-							placeholder="New Password"
-							update={setNewPassword}
-							type="password"
-						>
-							<UilEye color="#A0A3BD" />
-						</Input>
-						<Input
-							heading="Re-Enter New Password"
-							placeholder="New Password"
-							update={setReNewPassword}
-							type="password"
-						>
-							<UilEye color="#A0A3BD" />
-						</Input>
-					</div>
-					<div className="profilePic">
-						<div className="circleCard">
-							<img src={user.profileIMG} alt="" />
+				{user.googleID === "" && (
+					<div className="settingsName">
+						<div className="nameSize">
+							<div className="heading2">Password Change :</div>
+							<Input
+								heading="Current Password"
+								placeholder="Password"
+								update={setPassword}
+								type="password"
+							>
+								<UilEye color="#A0A3BD" />
+							</Input>
+							<Input
+								heading="New Password"
+								placeholder="New Password"
+								update={setNewPassword}
+								type="password"
+							>
+								<UilEye color="#A0A3BD" />
+							</Input>
+							<Input
+								heading="Re-Enter New Password"
+								placeholder="New Password"
+								update={setReNewPassword}
+								type="password"
+							>
+								<UilEye color="#A0A3BD" />
+							</Input>
 						</div>
+						<button
+							className="button"
+							type="button"
+							onClick={changePass}
+							style={{ marginTop: "2vh", width: "10vw" }}
+						>
+							Submit
+						</button>
 					</div>
-				</div>
+				)}
+
+				<div className="heading2">Delete Account :</div>
 
 				<button
 					className="button"
 					type="button"
-					onClick={changePass}
-					style={{ marginTop: "2vh", width: "10vw" }}
+					onClick={deleteAccount}
+					style={{
+						marginTop: "2vh",
+						width: "10vw",
+						border: "2px solid red",
+					}}
 				>
-					Submit
+					Delete Account
 				</button>
-			</form>
+			</div>
 		</div>
 	);
 };
